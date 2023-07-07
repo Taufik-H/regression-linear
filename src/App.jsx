11d130799f948b040accfd6216a06ef56cae1200
Taufik-H <taufikrpl1@gmail.com>
2023-07-07 09:26:16 +0700

    tampilan

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import * as math from 'mathjs';
 
-const WEATHER_API_KEY = process.env.VITE_WEATHER_API;
+const WEATHER_API_KEY = `${import.meta.env.VITE_WEATHER_API}`;
 const WEATHER_API_ENDPOINT = `https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=`;
 
 export default function App() {
@@ -26,6 +26,8 @@ export default function App() {
       const historicalData = data.forecast.forecastday.map((day) => ({
         date: day.date,
         temp: day.day.avgtemp_c,
+        condition: day.day.condition.text,
+        icon: day.day.condition.icon,
       }));
 
       const predictedWeather = applyRegressionAlgorithm(historicalData);
@@ -57,13 +59,25 @@ export default function App() {
     const tomorrowTemp = math.multiply(tomorrowFeatures, coefficients);
 
     // Additional logic to predict weather condition based on temperature
-    const condition =
-      tomorrowTemp >= 25 ? 'Cerah' : tomorrowTemp >= 15 ? 'Berawan' : 'Hujan';
+    let condition = '';
+    let icon = '';
+
+    if (tomorrowTemp >= 30) {
+      condition = 'Cerah';
+      icon = 'https://openweathermap.org/img/wn/01d.png';
+    } else if (tomorrowTemp >= 20) {
+      condition = 'Berawan';
+      icon = 'https://openweathermap.org/img/wn/02d.png';
+    } else {
+      condition = 'Hujan';
+      icon = 'https://openweathermap.org/img/wn/10d.png';
+    }
 
     const regressionResult = {
       coefficients,
       tomorrowTemp: tomorrowTemp.toFixed(2),
       condition,
+      icon,
     };
 
     setRegressionResult(regressionResult);
@@ -71,54 +85,50 @@ export default function App() {
     return {
       temp: regressionResult.tomorrowTemp,
       condition,
+      icon,
     };
   };
 
   return (
     <div className="bg-gradient-to-r from-blue-400 to-indigo-500 min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-md p-5">
-        <div className="bg-white shadow-md rounded px-8 py-6">
-          <h1 className="text-xl font-bold mb-4">Prediksi Cuaca</h1>
-          <div className="flex mb-4">
-            <input
-              className="appearance-none rounded-r-none bg-white border border-gray-300 rounded-md w-full py-2 px-3 text-gray-700  focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
-              type="text"
-              value={location}
-              onChange={handleLocationChange}
-              placeholder="Masukkan nama lokasi"
-            />
-            <button
-              className="bg-blue-500  hover:bg-blue-600 text-white font-bold py-2 px-3 rounded-l-none"
-              onClick={fetchWeatherData}
-              disabled={location === '' || loading}
-            >
-              {loading ? 'Memuat...' : 'Dapatkan Prediksi Cuaca'}
-            </button>
+      <div className="w-full max-w-md p-2">
+        <div className="bg-white bg-opacity-20 rounded-xl p-3 border border-gray-300 ">
+          <h1 className="text-xl font-bold mb-4 text-white">Prediksi Cuaca</h1>
+          <div className="flex flex-col mb-4">
+            <div className="flex items-center bg-white rounded-md py-2 px-3 mb-2">
+              <input
+                className="bg-white outline-none flex-grow text-slate-800 placeholder-slate-500"
+                type="text"
+                value={location}
+                onChange={handleLocationChange}
+                placeholder="Masukkan nama lokasi"
+              />
+              <button
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-3 rounded-lg"
+                onClick={fetchWeatherData}
+                disabled={location === '' || loading}
+              >
+                {loading ? 'Memuat...' : 'Prediksi'}
+              </button>
+            </div>
           </div>
           {loading && <p>Proses algoritma regresi sedang berjalan...</p>}
         </div>
-        {regressionResult && (
-          <div className="mt-4 bg-white shadow-md rounded px-8 py-6">
-            <h3 className="text-lg font-bold mb-2">Hasil Algoritma Regresi</h3>
-            <p className="mb-2">Rumus Algoritma Regresi:</p>
-            <code className="mb-2">
-              Y = {regressionResult.coefficients[0].toFixed(2)} +
-              {regressionResult.coefficients[1].toFixed(2)} * X
-            </code>
-            <p className="mb-2">
-              Nilai X (timestamp besok): {regressionResult.tomorrowTemp}
-            </p>
-            <p className="mb-2">
-              Prediksi suhu cuaca besok: {regressionResult.tomorrowTemp}°C
-            </p>
-            <p>Kondisi cuaca besok: {regressionResult.condition}</p>
-          </div>
-        )}
         {prediction && (
-          <div className="mt-4 bg-white shadow-md rounded px-8 py-6">
-            <p className="mb-4">
-              Prediksi cuaca besok: {prediction.temp}°C, {prediction.condition}
-            </p>
+          <div className="mt-4 bg-white bg-opacity-20 rounded-xl p-3 border border-gray-300">
+            <div className="flex items-center gap-3">
+              <div className="p-2 rounded-md bg-slate-600">
+                <img
+                  src={prediction.icon}
+                  alt="Weather Icon"
+                  className="w-20 h-20 mr-3 object-cover"
+                />
+              </div>
+              <p className="text-lg font-bold mb-4 text-white">
+                Prediksi cuaca besok: {prediction.temp}°C,{' '}
+                {prediction.condition}
+              </p>
+            </div>
           </div>
         )}
       </div>
